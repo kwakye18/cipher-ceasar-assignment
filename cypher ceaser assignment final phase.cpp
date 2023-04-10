a8157c23d72a424e259327206ea45c70e4e82fb0
kwakye stephen <kwakyestephen3@gmail.com>
2023-04-10 13:37:28 +0000

    some editing

--- cypher ceaser assignment final phase.cpp
+++ cypher ceaser assignment final phase.cpp
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-string encrypt(string message, int shift_count)
+string encrypt(string message, int shiftcount, string decryptedmessage, string encrypted_message)
 {
-    string encrypted_message = "";
+    encrypted_message = "";
 
     for (int i = 0; i < message.size(); i++) {
         if (isupper(message[i])) {
-        encrypted_message+=char(int(message[i] + shift_count - 65) % 26 + 65);
+        encrypted_message+=char(int(message[i] + shiftcount - 65) % 26 + 65);
         }
         else if (islower(message[i])) {
-            encrypted_message += char(int(message[i] + shift_count - 97) % 26 + 97);
+            encrypted_message += char(int(message[i] + shiftcount - 97) % 26 + 97);
         }
         else
-		 {
+   {
             encrypted_message += message[i];
         }
     }
@@ -23,22 +23,34 @@ string encrypt(string message, int shift_count)
     return encrypted_message;
 }
 
-string decrypt(string encrypted_message, int shift_count)
+string decrypt(string encryptedmessage, int shift_count)
 {
-    return encrypt(encrypted_message, 26 - shift_count);
+    return encrypt(encryptedmessage, shift_count, "", "");
 }
 
 int main()
 {
-    string message = "holla ";
-    int shift_count = 7;
+    // allowing user to enter some inputs
+    string message, decrypted_message, encrypted_message;
+    int shift_count;
 
-    string encrypted_message = encrypt(message, shift_count);
-    string decrypted_message = decrypt(encrypted_message, shift_count);
+    cout << "Enter the message you want to encrypt: ";
+    getline(cin, message);
 
-    cout << "initial message: " << message << endl;
-    cout << "encryptedmessage: " << encrypted_message << endl;
-    cout << "decryptedmessage: " << decrypted_message << endl;
+    cout << "Enter the shift count: ";
+    cin >> shift_count;
+
+    // call the encryption function
+    encrypted_message = encrypt(message, shift_count, decrypted_message, encrypted_message);
+
+    // print the encrypted message
+    cout << "Encrypted message: " << encrypted_message << endl;
+
+    // call the decryption function
+    decrypted_message = decrypt(encrypted_message, shift_count);
+
+    // print the decrypted message
+    cout << "Decrypted message: " << decrypted_message << endl;
 
     return 0;
 }
